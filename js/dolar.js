4c7daee0a0e3369529e163a9392f2e91bea2e6eb
fgaete94 <f.gaete@duocuc.cl>
2024-05-19 21:32:54 -0400

    se crea JS para obtencion de dolar y se agrega link para validar

--- js/dolar.js
+++ js/dolar.js
@@ -1,8 +1,50 @@
-fetch('https://mindicador.cl/api').then(function(response) {
-   return response.json();
-}).then(function(dailyIndicators) {
-  document.getElementById("dolar").innerHTML = 'US$' + dailyIndicators.dolar.valor;
+/* el primer ejemplo es solo usando una url especifica*/
+/*function getDolar(done){  Creamos un funcion que es para recuperar el valor del dolar
+  const results = fetch('https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar/2024/05/dias/17?apikey=2547195c57077b59253fcc77d3d749f2ad7d7469&formato=json');
+luego creamos una constante que le entregamos como valor el fetch que utilizamos, para luego lo que hacemos es que resultados lo formateamos como json
+  results .then(response => response.json())
+    .then(data => {done(data)})
+    .catch(error => { console.log('Error al consumir la API:',error);});
+}
 
-}).catch(function(error) {
-  console.log('Requestfailed', error);
+getDolar(data=> {
+  console.log(data)
+  const valorDolar = data.Dolares[0].Valor;
+  document.getElementById("dolar").innerHTML = 'US$' + valorDolar;
+
+
+});*/
+/*en este codigo a continuacion lo que hacemos es definir 2 URL, donde frente  un error le digo que use una URL secundaria para la obtencion de lo ue requiero*/
+function getDolar(done) {
+  const primaryUrl = 'https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar?apikey=2547195c57077b59253fcc77d3d749f2ad7d7469&formato=json';
+  const secondaryUrl = 'https://api.cmfchile.cl/api-sbifv3/recursos_api/dolar/2024/05/dias/17?apikey=2547195c57077b59253fcc77d3d749f2ad7d7469&formato=json'; // URL alternativa
+
+  function fetchDolar(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      });
+  }
+
+  fetchDolar(primaryUrl)
+    .then(data => done(data))
+    .catch(error => {
+      console.log('Error al consumir la API principal:', error);
+      // Intentar con la URL alternativa
+      fetchDolar(secondaryUrl)
+        .then(data => done(data))
+        .catch(error => {
+          console.log('Error al consumir la API alternativa:', error);
+        });
+    });
+}
+
+getDolar(data => {
+  console.log(data);
+  /*const valorDolar = data.Dolares[0].Valor;*/
+  document.getElementById("dolar").innerHTML = 'US$' + data.Dolares[0].Valor;
+  document.getElementById("fecha").innerHTML = ',Fecha Obtención:' + data.Dolares[0].Fecha;
 });
