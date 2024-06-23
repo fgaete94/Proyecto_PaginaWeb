ca09e1c954eeb805250fd2bd96de26a24d1e2de1
Julio Alejandro Cifuentes Gallardo <juli.cifuentes@duocuc.cl>
2024-06-23 17:55:39 -0400

    agregar_producto

--- enfqx/productos/urls.py
+++ enfqx/productos/urls.py
@@ -3,4 +3,5 @@ from productos import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('agregar_producto/',views.agregar_producto,name='agregar_producto'),
 ]
--- enfqx/productos/views.py
+++ enfqx/productos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Producto
+from .forms import ProductoForm
 
 # Create your views here.
 
@@ -7,3 +8,13 @@ def product_list(request):
     product = Producto.objects.all()
     context ={'product': product}
     return render (request,'productos/product_list.html',context)
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+        else:
+            form = ProductoForm()
+        return render(request,'productos/agregar_producto.html',{'form':form})
