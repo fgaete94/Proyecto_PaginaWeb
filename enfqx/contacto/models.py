eaf0985a5d88b3360d40d227ce398787d348a017
Ariel Olivares <ar.olivaresv@duocuc.cl>
2024-06-24 02:07:37 -0400

    Avanzando

--- enfqx/contacto/models.py
+++ enfqx/contacto/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.conf import settings
+# contacto/models.py
 
+from django.db import models
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -8,6 +8,6 @@ class Contacto(models.Model):
     rut = models.CharField(max_length=20)
     correo_electronico = models.EmailField(max_length=100)
     consulta = models.TextField()
-    
+
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} {self.apellido}"
--- enfqx/contacto/views.py
+++ enfqx/contacto/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactoForm
 
-def contacto(request):
-    context = {}
-    return render(request, 'contacto/contacto.html',context)
-
+def contacto_nuevo(request):
+    if request.method == "POST":
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Tu consulta ha sido enviada exitosamente.")
+            return redirect('contacto_nuevo')  
+    else:
+        form = ContactoForm()
+    return render(request, 'contacto/contacto_form.html', {'form': form})
