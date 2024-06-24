721b3a30f3726bae84a8232ed59bc71efff72b96
Ariel Olivares <ar.olivaresv@duocuc.cl>
2024-06-24 02:30:20 -0400

    .

--- enfqx/contacto/views.py
+++ enfqx/contacto/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import ContactoForm
 
-def contacto_nuevo(request):
+def contacto(request):
     if request.method == "POST":
         form = ContactoForm(request.POST)
         if form.is_valid():
@@ -11,4 +11,4 @@ def contacto_nuevo(request):
             return redirect('contacto_nuevo')  
     else:
         form = ContactoForm()
-    return render(request, 'contacto/contacto_form.html', {'form': form})
+    return render(request, 'contacto/contacto.html', {'form': form})
