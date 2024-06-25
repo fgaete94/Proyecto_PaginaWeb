5a5d82d7a5ad4ca7b54dd52be23ab75e52463af1
fgaete94 <f.gaete@duocuc.cl>
2024-06-25 18:29:15 -0400

    se crea qs y colab

--- enfqx/pagina/models.py
+++ enfqx/pagina/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Colab(models.Model):
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    image = models.ImageField(null=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Auth(models.Model):
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    image = models.ImageField(null=True)
+
+
+    def __str__(self):
+        return self.name
--- enfqx/pagina/views.py
+++ enfqx/pagina/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from publicacion.models import Post 
+from .models import Colab,Auth
 
 # Create your views here.
 
@@ -8,3 +9,12 @@ def index(request):
     context = {'recent_posts': recent_posts}
     return render (request,'pagina/index.htm', context)
 
+def colaboradores(request):
+    colab = Colab.objects.all()
+    context ={'colab': colab}
+    return render (request,'pagina/colaboradores.html',context)
+
+def autoras(request):
+    auth = Auth.objects.all()
+    context ={'auth': auth}
+    return render (request,'pagina/quienes_somos.html',context)
