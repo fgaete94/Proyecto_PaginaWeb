dc4ccde777997b8bfff06651de10381b8b8e5fd3
Ariel Olivares <ar.olivaresv@duocuc.cl>
2024-06-25 14:21:17 -0400

    estructura ordenada

--- enfqx/contacto/forms.py
+++ enfqx/contacto/forms.py
@@ -1,4 +1,12 @@
-# En contacto/forms.py
+# # En contacto/forms.py
+
+# from django import forms
+# from .models import Contacto
+
+# class ContactoForm(forms.ModelForm):
+#     class Meta:
+#         model = Contacto
+#         fields = ['nombre', 'apellido', 'rut', 'correo_electronico', 'consulta']
 
 from django import forms
 from .models import Contacto
@@ -7,4 +15,10 @@ class ContactoForm(forms.ModelForm):
     class Meta:
         model = Contacto
         fields = ['nombre', 'apellido', 'rut', 'correo_electronico', 'consulta']
-
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}),
+            'rut': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '11111111-1'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'name@example.com'}),
+            'consulta': forms.Textarea(attrs={'class': 'form-control', 'rows': 8, 'placeholder': 'Escribe tu consulta'}),
+        }
