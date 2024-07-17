f72c1611fb629fa0757e57d7135d2244c2d7ecf9
Julio Alejandro Cifuentes Gallardo <juli.cifuentes@duocuc.cl>
2024-07-16 20:07:36 -0400

    cerrar sesión listo

--- enfqx/productos/urls.py
+++ enfqx/productos/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('remove_from_cart/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
-     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Redirigir al índice 
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Redirigir al índice
     
 ]
-LOGOUT_REDIRECT_URL = 'index'
+#LOGOUT_REDIRECT_URL = 'index'
